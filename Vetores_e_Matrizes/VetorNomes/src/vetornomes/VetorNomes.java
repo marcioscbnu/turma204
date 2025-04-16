cc8a88c4dc51f9c5308e602906cd406aa731702d
marcioscbnu <marcio.schoenfelder@edu.sc.senai.br>
2025-04-16 11:55:39 -0300

    Versão com matrizes

--- Vetores_e_Matrizes/VetorNomes/src/vetornomes/VetorNomes.java
+++ Vetores_e_Matrizes/VetorNomes/src/vetornomes/VetorNomes.java
@@ -7,26 +7,33 @@ public class VetorNomes {
         /* Enunciado : crie um programa que armazene cinco nomes.
                        Ao final liste os nomes na horizontal separados por um hifen.
         */
-        String[] vetNomes  = new String[5];
-        Double[] vetMedias = new Double[5];
-        String[] vetSituacao = new String[5];
+        String[] vetNomes       = new String[5];
+        Double[] vetMedias      = new Double[5];
+        String[] vetSituacao    = new String[5];
         String[] vetResponsavel = new String[5];
+        Double[][] matNotas     = new Double[5][4];
         
         //Entrada de dados
         
-        for (int i = 0; i < 5; i++) {
+        for (int lin = 0; lin < 5; lin++) {
             System.out.print("Informe um nome : ");
-            vetNomes[i] = sc.next();            
+            vetNomes[lin] = sc.next();            
             System.out.print("Informe o responsavel : ");
-            vetResponsavel[i] = sc.next();
-            
+            vetResponsavel[lin] = sc.next();
             System.out.print("Informe a media do aluno : ");
-            vetMedias[i] = sc.nextDouble();
+            vetMedias[lin] = sc.nextDouble();
+            // matriz de notas
+            for (int col = 0; col < 4; col++) {
+                System.out.println("Informe a nota nr " + (col+1) +
+                                   " do " + vetNomes[lin]);
+                matNotas[lin][col] = sc.nextDouble();
+            }            
+            
             // se media >= 7 "APROVADO" senão "REPROVADO"
-            if (vetMedias[i] >= 7) {
-                vetSituacao[i] = "APROVADO";
+            if (vetMedias[lin] >= 7) {
+                vetSituacao[lin] = "APROVADO";
             } else {
-                vetSituacao[i] = "REPROVADO";
+                vetSituacao[lin] = "REPROVADO";
             }
         }
 
@@ -42,12 +49,16 @@ public class VetorNomes {
             }
         }
         */  
-        for (int pos = 0; pos< 5; pos++) {
-            System.out.println("Aluno.......: " + vetNomes[pos]);
-            System.out.println("Responsavel.: " + vetResponsavel[pos]);
-            System.out.println("Media.......: " + vetMedias[pos]);
-            System.out.println("Situacao....: " + vetSituacao[pos]);
-            System.out.println("-----------------------------------");
+        for (int lin = 0; lin < 5; lin++) {
+            System.out.println("Aluno.......: " + vetNomes[lin]);
+            System.out.println("Responsavel.: " + vetResponsavel[lin]);
+            System.out.println("Media.......: " + vetMedias[lin]);
+            System.out.println("Situacao....: " + vetSituacao[lin]);
+            System.out.print  ("Notas ......: ");
+            for (int col = 0; col < 4; col++) {
+                System.out.print(matNotas[lin][col] + " - ");
+            }
+            System.out.println("\n-----------------------------------");
         }
         System.out.println("");        
         sc.close();
